e855108d1a86938eba8c290f7193c57a015dbaa7
Steven Liu <hliu4@stuy.edu>
2018-01-06 22:07:49 -0500

    Completed threading

--- Snakes/Game.java
+++ Snakes/Game.java
@@ -1,14 +1,31 @@
 import java.util.*;
-public class Game{
-    ArrayList<Coordinate> ListofCoord;
-    Snake player;
-    Food Apple;
-    public void Initialize(){
-    }
-    public void Render(){
-    }
-    public void Tick(){
-    }
-    public static void main(String[] args){
-    }
+
+public class Game implements Runnable {
+	private Display display;
+	
+	int height, width;
+	String title;
+	boolean running;
+
+	private Thread thread;
+
+	public void run() {
+
+	}
+	public synchronized void start() {
+		if (running)
+			return;
+		thread = new Thread(this);
+		thread.start();
+	}
+	public synchronized void end() {
+		if (!running)
+			return;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
