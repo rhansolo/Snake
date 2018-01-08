a2d00d6ebbe3499017904169f690e0d29150e3dd
robinhanstuy <rhan@stuy.edu>
2018-01-08 14:41:41 -0500

    added segment arraylist + modified constructor

--- Snakes/Snake.java
+++ Snakes/Snake.java
@@ -1,7 +1,13 @@
+import java.util.*;
 public class Snake extends Entity{
     int startingLength;
-    String[] direction = ["UP","DOWN","LEFT","RIGHT"];
+    String[] direction = {"UP","DOWN","LEFT","RIGHT"};
+    ArrayList<Segment> segmentList= new ArrayList<Segment>(3);
     public Snake(){
 	startingLength = 3;
+	segmentList.set(0,new Segment(0,0));
+	segmentList.set(1,new Segment(1,0));
+	segmentList.set(2,new Segment(2,0));
     }
+    
 }
