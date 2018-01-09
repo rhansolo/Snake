25bcadbd534ce6b7aab7ff2ed5920279d44b5804
robinhanstuy <rhan@stuy.edu>
2018-01-08 21:21:03 -0500

    added entity abstract class

---
+++ Snakes/Entity.java
@@ -0,0 +1,24 @@
+public class Entity{
+    private int x,y;
+
+    public int getX(){
+	return x;
+    }
+    public int getY(){
+	return y;
+    }
+    public void setX(int xcor){
+	x = xcor;
+    }
+    public void setY(int ycor){
+	y = ycor;
+    }
+    public void setPostion(int xcor, int ycor){
+	x = xcor;
+	y = ycor;
+    }
+    public void move(int xChange, int yChange){
+	x += xChange;
+	y += yChange;
+    }
+
