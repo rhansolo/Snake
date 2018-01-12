164c608ec39f6f9f357f593f5a3f112d84249ce6
robinhanstuy <rhan@stuy.edu>
2018-01-11 23:14:50 -0500

    fixed errors in game.java and changed keyboard events to suit it

--- Snakes/KeyboardEvents.java
+++ Snakes/KeyboardEvents.java
@@ -6,46 +6,30 @@ import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class KeyboardEvents extends JPanel {
+public class KeyboardEvents implements KeyListener {
     public String direction;
-	  public KeyboardEvents() {
-		    KeyListener listener = new MyKeyListener();
-		      addKeyListener(listener);
-		        setFocusable(true);
-	  }
-
-	  public static void main(String[] args) {
-		    JFrame frame = new JFrame("test");
-		    KeyboardEvents  keyboardExample = new KeyboardEvents();
-		    frame.add(keyboardExample);
-		    frame.setSize(200, 200);
-		    frame.setVisible(true);
-		    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	  }
-
-	  public class MyKeyListener implements KeyListener {
-		    public void keyTyped(KeyEvent e) {
-		      }
-		    public void keyPressed(KeyEvent e) {
-		    //String key = KeyEvent.getKeyText(e.getKeyCode());
-		    if (KeyEvent.getKeyText(e.getKeyCode()) == "Space"){
-			       System.exit(0);
-		    }
-		    else if (KeyEvent.getKeyText(e.getKeyCode()).equals("W") ){
-			       direction = "UP";
-		    }
-		    else if (KeyEvent.getKeyText(e.getKeyCode()).equals("A")){
-			       direction = "LEFT";
-		    }
-		    else if (KeyEvent.getKeyText(e.getKeyCode()).equals("S")){
-			       direction = "DOWN";
-		    }
-		    else if (KeyEvent.getKeyText(e.getKeyCode()).equals("D")){
-			       direction = "RIGHT";
-		    }
-		    System.out.println(direction);
-		}
-		public void keyReleased(KeyEvent e) {
-		}
+    public void keyTyped(KeyEvent e) {
+    }
+    public static void tick(){
+	
+    }
+    public void keyPressed(KeyEvent e) {
+	if (KeyEvent.getKeyText(e.getKeyCode()) == "Space"){
+	    System.exit(0);
 	}
+	else if (KeyEvent.getKeyText(e.getKeyCode()).equals("W") ){
+	    direction = "UP";
+	}
+	else if (KeyEvent.getKeyText(e.getKeyCode()).equals("A")){
+	    direction = "LEFT";
+	}
+	else if (KeyEvent.getKeyText(e.getKeyCode()).equals("S")){
+	    direction = "DOWN";
+	}
+	else if (KeyEvent.getKeyText(e.getKeyCode()).equals("D")){
+	    direction = "RIGHT";
+	}
+    }
+    public void keyReleased(KeyEvent e) {
+    }
 }
