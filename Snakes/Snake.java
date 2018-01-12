d89356573a8107475c961027ffd3f11853e79e34
robinhanstuy <rhan@stuy.edu>
2018-01-11 22:53:30 -0500

    fixed bugs in snake.java, bugs still exist in game.java

--- Snakes/Snake.java
+++ Snakes/Snake.java
@@ -1,44 +1,51 @@
 import java.util.*;
 import java.awt.Graphics;
+import java.awt.Color;
 
 public class Snake extends Entity{
     private int startingLength;
-
+    
     private ArrayList<Coordinate> coordList= new ArrayList<Coordinate>(3);
     private Game game;
 
-	  public Snake(Game game, float x, float y) {
-		    super(x, y);
-		    this.game = game;
-        startingLength = 3;
-        segmentList.set(0,new Segment(0,0));
-        segmentList.set(1,new Segment(1,0));
-        segmentList.set(2,new Segment(2,0));
-	  }
+    public Snake(Game game, float x, float y) {
+	super(x, y);
+	this.game = game;
+	startingLength = 3;
+	coordList.set(0,new Coordinate(0,0));
+	coordList.set(1,new Coordinate(1,0));
+	coordList.set(2,new Coordinate(2,0));
+    }
     
-	  @Override
-	  public void tick() {
-	    	Coordinate newHead;
-		  if(game.getKeyManager().direction.equals("RIGHT"))
-		      newHead = new Coordinate(coordList.get(0).getXcor()+1,coordList.get(0).getYcor());
-		  else if(game.getKeyManager().direction.equals("LEFT"))
-		      newHead = new Coordinate(coordList.get(0).getXcor()-1,coordList.get(0).getYcor());
-		  else if(game.getKeyManager().direction.equals("UP"))
-		      newHead = new Coordinate(coordList.get(0).getXcor(),coordList.get(0).getYcor()+1);
-		  else if(game.getKeyManager().direction.equals("DOWN"))
-		      newHead = new Coordinate(coordList.get(0).getXcor(),coordList.get(0).getYcor()-1);
-		  coordList.add(0,newHead);
-		  coordList.remove(coordList.size() - 1);
-	      }
-	   }
-
-	  @Override
-	  public void render(Graphics g) {
-      g.setColor(Color.GREEN);
-      for (Coordinate c: coordList){
-        g.fillRect(c.x * BOX_WIDTH, c.y * BOX_HEIGHT, BOX_WIDTH, BOX_HEIGHT);
-      }
-      g.setColor(Color.BLACK);
-	  }
-
+    public void tick() {
+	Coordinate newHead;
+	if(game.getKeyManager().direction.equals("RIGHT")){
+	    newHead = new Coordinate(coordList.get(0).getXcor()+1,coordList.get(0).getYcor());
+	    coordList.add(0,newHead);
+	    coordList.remove(coordList.size() - 1);
+	}
+	else if(game.getKeyManager().direction.equals("LEFT")){
+	    newHead = new Coordinate(coordList.get(0).getXcor()-1,coordList.get(0).getYcor());
+	    coordList.add(0,newHead);
+	    coordList.remove(coordList.size() - 1);
+	}
+	else if(game.getKeyManager().direction.equals("UP")){
+	    newHead = new Coordinate(coordList.get(0).getXcor(),coordList.get(0).getYcor()+1);
+	    coordList.add(0,newHead);
+	    coordList.remove(coordList.size() - 1);
+	}
+	else if(game.getKeyManager().direction.equals("DOWN")){
+	    newHead = new Coordinate(coordList.get(0).getXcor(),coordList.get(0).getYcor()-1);
+	    coordList.add(0,newHead);
+	    coordList.remove(coordList.size() - 1);
+	}
+    }
+    public void render(Graphics g) {
+	g.setColor(Color.GREEN);
+	for (Coordinate c: coordList){
+	    g.fillRect(c.getXcor() * 10, c.getYcor() * 10, 10, 10);
+	}
+	g.setColor(Color.BLACK);
+    }
+    
 }
