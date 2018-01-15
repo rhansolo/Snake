6f46f328502e33ce14ff03f4774d224df51988ea
Steven Liu <hliu4@stuy.edu>
2018-01-15 16:57:34 -0500

    Added ScoreBoard and lives (not fully functional yet

--- Snakes/Display.java
+++ Snakes/Display.java
@@ -1,12 +1,14 @@
-import java.awt.Canvas;
-import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.*;
 
-import javax.swing.JFrame;
+import java.awt.EventQueue;
+import javax.swing.*;
 public class Display {
 
 	private JFrame frame;
 	private Canvas canvas;
+
+	private JTextField txtCurrentScore;
+	private JTextField txtLives;
 
 	private String title;
 	private int width, height;
@@ -26,6 +28,25 @@ public class Display {
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+
+		// Textfield here
+		txtCurrentScore = new JTextField();
+		txtCurrentScore.setBackground(UIManager.getColor("CheckBox.background"));
+		txtCurrentScore.setForeground(Color.GREEN);
+		txtCurrentScore.setFont(new Font("Verdana", Font.BOLD, 38));
+		txtCurrentScore.setText("Current Score:");
+		txtCurrentScore.setBounds(0, 0, 726, 49);
+		frame.getContentPane().add(txtCurrentScore);
+		txtCurrentScore.setColumns(10);
+
+		txtLives = new JTextField();
+		txtLives.setForeground(Color.GREEN);
+		txtLives.setFont(new Font("Verdana", Font.BOLD, 38));
+		txtLives.setBackground(UIManager.getColor("CheckBox.background"));
+		txtLives.setText("Lives:");
+		txtLives.setBounds(721, 0, 319, 49);
+		frame.getContentPane().add(txtLives);
+		txtLives.setColumns(10);
 
 		canvas = new Canvas();
 		canvas.setPreferredSize(new Dimension(width, height));
