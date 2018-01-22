ecc294c474476f84a887e084bc59155a94937d7b
Steven Liu <hliu4@stuy.edu>
2018-01-21 20:56:50 -0500

    Fixed errors

--- Snakes/Apple.java
+++ Snakes/Apple.java
@@ -10,10 +10,10 @@ public class Apple {
       // generates a randomn location for the apple to spawn
       xCor = rand.nextInt((game.width + 1));
       yCor = rand.nextInt((game.height + 1));
-      while ((xCor / 10 < 1) || (xCor / 10 > game.height / 10 - 1)){
+      while ((xCor / 10 < 10) || (xCor / 10 > game.width / 10 - 5)){
         xCor = rand.nextInt((game.width + 1));
       }
-      while ((yCor / 10 > game.width / 10 - 5 )|| (yCor / 10 < 9)){
+      while ((yCor / 10 > game.height / 10 - 5 )|| (yCor / 10 < 9)){
         yCor = rand.nextInt((game.width + 1));
       }
 	    this.game = game;
